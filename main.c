7618a40e91a1ffddf8c866f4e02bba8c2b867324
AdrianoDias <adrianojeronimofilho@hotmail.com>
2024-08-18 10:39:19 -0300

    Filas

--- main.c
+++ main.c
@@ -14,18 +14,18 @@ int main() {
     inserir(&fila, 40);
     inserir(&fila, 50);
 
-    // Testando se a fila está cheia
+    // Testando se a fila esta cheia
     if (estaCheia(&fila)) {
-        printf("A fila está cheia.\n");
+        printf("A fila esta cheia.\n");
     } else {
-        printf("A fila não está cheia.\n");
+        printf("A fila nao esta cheia.\n");
     }
 
-    // Testando a consulta ao início da fila
+    // Testando a consulta ao inicio da fila
     if (consultarInicio(&fila, &valor)) {
-        printf("O início da fila é: %d\n", valor);
+        printf("O inicio da fila e: %d\n", valor);
     } else {
-        printf("A fila está vazia.\n");
+        printf("A fila esta vazia.\n");
     }
 
     // Testando a remoção
@@ -33,16 +33,16 @@ int main() {
     printf("Valor removido: %d\n", valor);
 
     if (consultarInicio(&fila, &valor)) {
-        printf("O início da fila é: %d\n", valor);
+        printf("O inicio da fila e: %d\n", valor);
     } else {
-        printf("A fila está vazia.\n");
+        printf("A fila esta vazia.\n");
     }
 
-    // Testando se a fila está vazia
+    // Testando se a fila esta vazia
     if (estaVazia(&fila)) {
-        printf("A fila está vazia.\n");
+        printf("A fila esta vazia.\n");
     } else {
-        printf("A fila não está vazia.\n");
+        printf("A fila nao esta vazia.\n");
     }
 
     return 0;
